fd734be5b022c0b2672e3fa1ca7c376dd9e801f2
Armaan Gupta <ohayouarmaan@gmail.com>
2022-12-24 22:58:40 +0530

    Added fire trail

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ cards.forEach((card, index) => {
             card.classList.add('active');
         } else {
             card.classList.add('inactive');
+            card.classList.remove('active');
         }
         Flip.from(state, {
             duration: 1.0,
@@ -26,6 +27,41 @@ cards.forEach((card, index) => {
 });
 
 const colorChange = document.querySelector('.change__color__theme');
+
+
+const circles = document.querySelectorAll(".circle");
+const coords = { x: 0, y: 0 };
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e"
+];
+
+circles.forEach(function (circle, index) {
+    circle.x = 0;
+    circle.y = 0;
+    circle.style.backgroundColor = colors[index % colors.length];
+});
+
 colorChange.addEventListener('click', e => {
     const darkTheme = {
         "background": "#1F2636",
@@ -41,6 +77,10 @@ colorChange.addEventListener('click', e => {
         "slate-text-color": "black"
     }
 
+
+    const darkColors = ['#000000', '#36454F', '#023020', '#301934', '#343434', '#1B1212', '#28282B', '#191970', '#353935'];
+
+
     if(colorChange.dataset.theme == 'dark') {
         Object.keys(lightTheme).forEach(key => {
             console.log(key, lightTheme[key])
@@ -48,6 +88,10 @@ colorChange.addEventListener('click', e => {
         });
         document.querySelector(".change__color__image").setAttribute("src", "./icons8-sunset-50.png")
         colorChange.dataset.theme = 'light';
+        circles.forEach(function (circle, index) {
+            circle.style.backgroundColor = darkColors[index % darkColors.length];
+            console.log(darkColors[index % darkColors.length])
+        });
     }
     else if(colorChange.dataset.theme == 'light') {
         Object.keys(darkTheme).forEach(key => {
@@ -56,6 +100,10 @@ colorChange.addEventListener('click', e => {
         });
         document.querySelector(".change__color__image").setAttribute("src", "./icons8-sunset-80.png")
         colorChange.dataset.theme = 'dark';
+        console.log("hello");
+        circles.forEach(function (circle, index) {
+            circle.style.backgroundColor = colors[index % colors.length];
+        });
     }
 
 })
@@ -72,3 +120,37 @@ window.addEventListener("scroll", e => {
     const whiteLine = document.querySelector('.white__line')
     whiteLine.style.height = `${getScrollPercent()}vh`
 })
+
+
+
+
+
+
+window.addEventListener("mousemove", function(e){
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+});
+
+function animateCircles() {
+  
+    let x = coords.x;
+    let y = coords.y;
+  
+    circles.forEach(function (circle, index) {
+      circle.style.left = window.scrollX + x - 12 + "px";
+      circle.style.top = window.scrollY + (y) - 12 + "px";
+    
+      circle.style.scale = (circles.length - index) / circles.length;
+    
+      circle.x = x;
+      circle.y = y;
+
+      const nextCircle = circles[index + 1] || circles[0];
+      x += (nextCircle.x - x) * 0.3;
+      y += (nextCircle.y - y) * 0.3;
+    });
+ 
+    requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
